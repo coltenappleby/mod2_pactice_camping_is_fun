d847930b17636e3be6d5bcd66e45c10f53518cb1
coltenappleby <colten.appleby@gmail.com>
2021-03-23 19:33:24 -0400

    pretty much completed

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -6,4 +6,24 @@ class CampersController < ApplicationController
   def show
     @camper = Camper.find(params[:id])
   end
+
+  def new
+    @camper = Camper.new
+  end
+
+  def create 
+    @camper = Camper.new(camper_params)
+    if @camper.valid?
+      redirect_to camper_path(@camper)
+    else
+      redirect_to new_camper_path()
+    end
+
+  end
+
+  private
+
+  def camper_params
+    params.require(:camper).permit(:name, :age)
+  end
 end
